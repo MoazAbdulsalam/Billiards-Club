6a734a7cd4807be2c0d99762053564c320cb906a
Moaz Abdulsalam <dod998849@gmail.com>
2026-08-09 09:33:01 +0300

    DTOs

---
+++ BilliardsDataAccessLayer/DTOs/PaymentDTO.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BilliardsDataAccessLayer.DTOs
+{
+    public class PaymentDTO
+    {
+        public int PaymentID { get; set; }
+        public int PlayerID { get; set; }
+        public int TableID { get; set; }
+        public DateTime StartTime { get; set; }
+        public DateTime? EndTime { get; set; }
+        public DateTime? DateOfPayment { get; set; }
+        public int CreatedByUserID { get; set; }
+        public decimal? Fees { get; set; }
+    }
+}
---
+++ BilliardsDataAccessLayer/DTOs/PoolTableDTO.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BilliardsDataAccessLayer.DTOs
+{
+    public class PoolTableDTO
+    {
+        public int TableID { get; set; }
+        public int TableNumber { get; set; }
+        public decimal PaymentPerHour { get; set; }
+    }
+
+    public class PoolTableStatusDTO
+    {
+        public PoolTableDTO Table { get; set; }
+
+        public bool IsAvailable { get; set; }
+
+        public int? CurrentPaymentID { get; set; }
+        public int? CurrentPlayerID { get; set; }
+        public string? CurrentPlayerName { get; set; }
+        public DateTime? StartTime { get; set; }
+    }
+}
