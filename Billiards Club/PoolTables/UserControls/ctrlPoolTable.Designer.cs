27aa83a7a4f10259fad1da9a1a5ce35b1078a0ed
Moaz Abdulsalam <dod998849@gmail.com>
2026-08-09 09:34:46 +0300

    PoolTable UserControl

--- Billiards Club/PoolTables/UserControls/ctrlPoolTable.Designer.cs
+++ Billiards Club/PoolTables/UserControls/ctrlPoolTable.Designer.cs
@@ -31,10 +31,11 @@
             components = new System.ComponentModel.Container();
             pictureBox1 = new PictureBox();
             gbTable = new GroupBox();
+            lblAvailable = new Label();
             lblPlayer = new Label();
             lblTimer = new Label();
             btnEnd = new Button();
-            btnStartStop = new Button();
+            btnStart = new Button();
             timer1 = new System.Windows.Forms.Timer(components);
             ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
             gbTable.SuspendLayout();
@@ -45,68 +46,84 @@
             pictureBox1.Image = Billiards_Club.Properties.Resources.pool;
             pictureBox1.Location = new Point(19, 70);
             pictureBox1.Name = "pictureBox1";
-            pictureBox1.Size = new Size(238, 162);
+            pictureBox1.Size = new Size(260, 166);
             pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
             pictureBox1.TabIndex = 0;
             pictureBox1.TabStop = false;
             // 
             // gbTable
             // 
-            gbTable.BackColor = Color.Black;
+            gbTable.BackColor = Color.FromArgb(242, 232, 242);
+            gbTable.Controls.Add(lblAvailable);
             gbTable.Controls.Add(lblPlayer);
             gbTable.Controls.Add(lblTimer);
             gbTable.Controls.Add(btnEnd);
-            gbTable.Controls.Add(btnStartStop);
+            gbTable.Controls.Add(btnStart);
             gbTable.Controls.Add(pictureBox1);
-            gbTable.ForeColor = SystemColors.ControlLightLight;
+            gbTable.Font = new Font("Segoe UI", 12F);
+            gbTable.ForeColor = Color.SlateBlue;
             gbTable.Location = new Point(3, 3);
             gbTable.Name = "gbTable";
-            gbTable.Size = new Size(384, 291);
+            gbTable.Size = new Size(417, 306);
             gbTable.TabIndex = 1;
             gbTable.TabStop = false;
             gbTable.Text = "Table";
             // 
+            // lblAvailable
+            // 
+            lblAvailable.AutoSize = true;
+            lblAvailable.Font = new Font("Segoe UI", 14F);
+            lblAvailable.ForeColor = Color.Green;
+            lblAvailable.Location = new Point(306, 244);
+            lblAvailable.Name = "lblAvailable";
+            lblAvailable.Size = new Size(98, 32);
+            lblAvailable.TabIndex = 5;
+            lblAvailable.Text = "Availble";
+            // 
             // lblPlayer
             // 
             lblPlayer.AutoSize = true;
-            lblPlayer.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
-            lblPlayer.Location = new Point(146, 23);
+            lblPlayer.Font = new Font("Segoe UI", 16F);
+            lblPlayer.ForeColor = Color.Navy;
+            lblPlayer.Location = new Point(19, 30);
             lblPlayer.Name = "lblPlayer";
-            lblPlayer.Size = new Size(65, 28);
+            lblPlayer.Size = new Size(89, 37);
             lblPlayer.TabIndex = 4;
             lblPlayer.Text = "Player";
             // 
             // lblTimer
             // 
             lblTimer.AutoSize = true;
-            lblTimer.Font = new Font("Segoe UI", 16.2F, FontStyle.Regular, GraphicsUnit.Point, 0);
-            lblTimer.Location = new Point(78, 239);
+            lblTimer.Font = new Font("Segoe UI", 24F);
+            lblTimer.Location = new Point(64, 244);
             lblTimer.Name = "lblTimer";
-            lblTimer.Size = new Size(119, 38);
+            lblTimer.Size = new Size(173, 54);
             lblTimer.TabIndex = 3;
             lblTimer.Text = "00:00:00";
             // 
             // btnEnd
             // 
-            btnEnd.BackColor = Color.Black;
-            btnEnd.Location = new Point(271, 131);
+            btnEnd.BackColor = Color.SlateBlue;
+            btnEnd.ForeColor = Color.White;
+            btnEnd.Location = new Point(285, 131);
             btnEnd.Name = "btnEnd";
-            btnEnd.Size = new Size(94, 43);
+            btnEnd.Size = new Size(119, 56);
             btnEnd.TabIndex = 2;
             btnEnd.Text = "End";
             btnEnd.UseVisualStyleBackColor = false;
             btnEnd.Click += btnEnd_Click;
             // 
-            // btnStartStop
+            // btnStart
             // 
-            btnStartStop.BackColor = Color.Black;
-            btnStartStop.Location = new Point(271, 70);
-            btnStartStop.Name = "btnStartStop";
-            btnStartStop.Size = new Size(94, 43);
-            btnStartStop.TabIndex = 1;
-            btnStartStop.Text = "Start";
-            btnStartStop.UseVisualStyleBackColor = false;
-            btnStartStop.Click += btnStart_Click;
+            btnStart.BackColor = Color.SlateBlue;
+            btnStart.ForeColor = Color.White;
+            btnStart.Location = new Point(285, 70);
+            btnStart.Name = "btnStart";
+            btnStart.Size = new Size(119, 55);
+            btnStart.TabIndex = 1;
+            btnStart.Text = "Start";
+            btnStart.UseVisualStyleBackColor = false;
+            btnStart.Click += btnStart_Click;
             // 
             // timer1
             // 
@@ -117,11 +134,11 @@
             // 
             AutoScaleDimensions = new SizeF(120F, 120F);
             AutoScaleMode = AutoScaleMode.Dpi;
-            BackColor = Color.Black;
+            BackColor = Color.FromArgb(242, 232, 242);
             Controls.Add(gbTable);
             ForeColor = SystemColors.ButtonHighlight;
             Name = "ctrlPoolTable";
-            Size = new Size(404, 317);
+            Size = new Size(423, 316);
             Load += ctrlPoolTable_Load;
             ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
             gbTable.ResumeLayout(false);
@@ -133,10 +150,11 @@
 
         private PictureBox pictureBox1;
         private GroupBox gbTable;
-        private Button btnStartStop;
+        private Button btnStart;
         private Label lblTimer;
         private Button btnEnd;
         private Label lblPlayer;
         private System.Windows.Forms.Timer timer1;
+        private Label lblAvailable;
     }
 }
--- Billiards Club/PoolTables/UserControls/ctrlPoolTable.cs
+++ Billiards Club/PoolTables/UserControls/ctrlPoolTable.cs
@@ -1,95 +1,170 @@
-﻿using System;
+﻿using Billiards_Club.Players;
+using BilliardsBusinessLayer;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Text;
 using System.Windows.Forms;
-
+using Billiards_Club.Players.Controls;
+using Billiards_Club.Classes;
 namespace Pool_Club
 {
     public partial class ctrlPoolTable : UserControl
     {
         public class TableEventArgs : EventArgs
         {
-            public int TotalSeconds { get; }
-            public string TimeConsumed { get; }
-            public double HourlyRate { get; }
-            public double Fees()
+            public clsPlayer Player { get; }
+            public clsPayment Payment { get; }
+            public clsPoolTable Table { get; }
+            public TableEventArgs(clsPlayer player, clsPayment payment, clsPoolTable table)
             {
-                return ((double)TotalSeconds / (60 * 60)) * HourlyRate;
-            }
-            public TableEventArgs(double HourlyRate, int TotalSeconds, string TimeConsumed)
-            {
-                this.HourlyRate = HourlyRate;
-                this.TotalSeconds = TotalSeconds;
-                this.TimeConsumed = TimeConsumed;
+                Player = player;
+                Payment = payment;
+                Table = table;
             }
         }
         public event EventHandler<TableEventArgs> TableFinished;
+        public event EventHandler<TableEventArgs> TableSelected;
+        public void RaiseTableSelected(TableEventArgs e)
+        {
+            TableSelected?.Invoke(this, e);
+        }
         public void RaiseTableFinished(TableEventArgs e)
         {
             TableFinished?.Invoke(this, e);
         }
 
-
-        double _HourlyRate = 10;
-        string _playerName="Player";
-        string _TableName;
-        int _Seconds = 0;
-        [ Category("Pool Configeration."), Description("Player Name") ]
-        [DesignerSerializationVisibility(DesignerSerializationVisibility.Visible)]
-        public string PlayerName { get { return _playerName; } set { lblPlayer.Text = value; _playerName = value; Invalidate(); } }
-        [Category("Pool Configeration."), Description("Table Name")]
-        [DesignerSerializationVisibility(DesignerSerializationVisibility.Visible)]
-        public string TableName { get { return _TableName; } set { gbTable.Text = value; _TableName = value; Invalidate(); } }
-        [Category("Pool Configeration."), Description("Rate Per Hour")]
-        [DesignerSerializationVisibility(DesignerSerializationVisibility.Visible)]
-        public double HourlyRate { get { return _HourlyRate; } set { _HourlyRate = value; } }
-
-
-        public ctrlPoolTable()
+        clsPlayer? Player;
+        public clsPoolTableStatus PoolTableStatus { get; private set; }
+        public ctrlPoolTable(clsPoolTableStatus tableStatus)
         {
             InitializeComponent();
-        }
-
-        private void timer1_Tick(object sender, EventArgs e)
-        {
-            _Seconds++;
-            TimeSpan time = TimeSpan.FromSeconds(_Seconds);
-            lblTimer.Text = time.ToString(@"hh\:mm\:ss");
-            lblTimer.Refresh();
+            PoolTableStatus = tableStatus;
 
         }
-
-        private void btnStart_Click(object sender, EventArgs e)
+        void UpdateUI()
         {
-            if (btnStartStop.Text == "Start")
+            gbTable.Text = "Table " + PoolTableStatus.Table.TableNumber;
+            lblPlayer.Text = PoolTableStatus.CurrentPlayerName ?? "Player";
+            btnStart.Enabled = PoolTableStatus.IsAvailable;
+            lblAvailable.Text = PoolTableStatus.IsAvailable ? "Available" : "Occupied";
+            lblAvailable.ForeColor = PoolTableStatus.IsAvailable ? Color.Green : Color.Red;
+            if (PoolTableStatus.StartTime.HasValue)
             {
-                btnStartStop.Text = "Stop";
-                timer1.Start();
+                TimeSpan elapsedTime = DateTime.Now - PoolTableStatus.StartTime.Value;
+                lblTimer.Text = elapsedTime.ToString(@"hh\:mm\:ss");
             }
             else
             {
-                btnStartStop.Text = "Start";
-                timer1.Stop();
+                lblTimer.Text = "00:00:00";
             }
         }
-
-        private void btnEnd_Click(object sender, EventArgs e)
+        private void OnPlayerSelected(object sender, Billiards_Club.Players.Controls.ctrlPlayerCardWithFilter.OnPlayerSelectedEventArgs e)
         {
-            timer1.Stop();
-            RaiseTableFinished(new TableEventArgs(HourlyRate, _Seconds, lblTimer.Text));
-            lblPlayer.Text = "Player";
-            lblTimer.Text = "00:00:00";
-            btnStartStop.Text = "Start";
-            _Seconds = 0;
+            Player = e.SelectedPlayer;
+            if(Player!=null)
+            {
+                MessageBox.Show($"Player {Player.PersonInfo.FullName()} selected.", "Player Selected", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                ((Form)sender).Close();
+            }
+        }
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (!PoolTableStatus.StartTime.HasValue)
+                return;
+
+            TimeSpan elapsed =
+                DateTime.Now - PoolTableStatus.StartTime.Value;
+
+            lblTimer.Text = elapsed.ToString(@"hh\:mm\:ss");
 
         }
-
-        private void ctrlPoolTable_Load(object sender, EventArgs e)
+        private async void btnStart_Click(object sender, EventArgs e)
         {
+            if (!PoolTableStatus.IsAvailable)
+            {
+                MessageBox.Show("The session has already started.", "Session Already Started", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+            Player = null;
+            using (frmSelectPlayer frm = new frmSelectPlayer())
+            {
+                frm.OnPlayerSelected += OnPlayerSelected;
+                frm.ShowDialog();
 
+            }
+            int? PaymentID = null;
+
+            if (Player != null)
+            {
+                  PaymentID = await PoolTableStatus.StartSessionAsync(Player, clsGlobals.CurrentUser.UserID.Value);
+
+                if (PaymentID == null)
+                {
+                    MessageBox.Show("Failed to start the session.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    return;
+                }
+            }
+            else
+            {
+                MessageBox.Show("Please select a player to start the session.", "Player Not Selected", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+            PoolTableStatus.CurrentPlayerID = Player.PlayerID.Value;
+            PoolTableStatus.CurrentPlayerName = Player.PersonInfo.FullName();
+            UpdateUI();
+            timer1.Start();
+            clsPayment payment = await clsPayment.GetPaymentByPaymentIDAsync(PaymentID.Value);
+            RaiseTableSelected(new TableEventArgs(Player, payment, PoolTableStatus.Table));
+    
+        }
+        private async void btnEnd_Click(object sender, EventArgs e)
+        {
+            if (PoolTableStatus.IsAvailable)
+            {
+                MessageBox.Show("The session has not started yet.", "Session Not Started", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+
+            double totalAmount = PoolTableStatus.Table.CalculateFees(PoolTableStatus.StartTime.Value, DateTime.Now);
+            if (MessageBox.Show($"Total amount due: ${totalAmount:F2}\nAre you sure you want to end the session?", "End Session", MessageBoxButtons.YesNo, MessageBoxIcon.Question) != DialogResult.Yes)
+                return;
+
+            timer1.Stop();
+            if (await PoolTableStatus.EndSessionAsync())
+            {
+                clsPayment? payment = await clsPayment.GetPaymentByPaymentIDAsync(PoolTableStatus.CurrentPaymentID.Value);
+                RaiseTableFinished( new TableEventArgs( Player,payment,PoolTableStatus.Table) );
+                PoolTableStatus.ResetTable();
+                Player = null;
+                UpdateUI();
+                MessageBox.Show("Session ended successfully.", "Session Ended", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+            }
+            else
+            {
+                timer1.Start();
+                MessageBox.Show("Failed to end the session.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            
+        }
+        private async void ctrlPoolTable_Load(object sender, EventArgs e)
+        {
+            if (PoolTableStatus.CurrentPlayerID.HasValue)
+            {
+                Player = await clsPlayer.FindPlayerByPersonIDAsync(PoolTableStatus.CurrentPlayerID.Value);
+            }
+            UpdateUI();
+
+            if(!PoolTableStatus.IsAvailable)
+            {
+                timer1.Start();
+            }
         }
     }
 }
